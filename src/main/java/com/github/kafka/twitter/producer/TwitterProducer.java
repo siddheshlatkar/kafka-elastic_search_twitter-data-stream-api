2f273dd6519e04937dbdc9ea10e8783bbdf901e4
Siddhesh <siddheshlatkar@gmail.com>
2020-07-26 15:18:15 -0400

    Refactoring

--- src/main/java/com/github/kafka/twitter/producer/TwitterProducer.java
+++ src/main/java/com/github/kafka/twitter/producer/TwitterProducer.java
@@ -1,4 +1,4 @@
-package com.github.twitter;
+package com.github.kafka.twitter.producer;
 
 import com.google.common.collect.Lists;
 
@@ -19,8 +19,6 @@ import org.apache.kafka.clients.producer.RecordMetadata;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import sun.awt.windows.ThemeReader;
 
 import java.util.List;
 import java.util.Properties;
